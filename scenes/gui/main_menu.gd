7f3db0bf0ce09e7c780621b30425c6ddf7c2f780
soixante <xdimartino@fastmail.fm>
2025-09-12 00:05:42 +0200

    menu

---
+++ scenes/gui/main_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+class_name menu
+
+func _ready() -> void:
+	pass
+
+func _init() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
---
+++ scenes/gui/sentence.gd
@@ -0,0 +1,5 @@
+extends RichTextLabel
+
+func _ready() -> void:
+	var wobble = WobbleEffect.new()
+	install_effect(wobble)
